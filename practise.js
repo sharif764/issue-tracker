54e34ee627cb83ede7e748ef462540eb13947ed0
sharif764 <ss1884088@gmail.com>
2021-09-09 02:27:15 +0600

    JJ

---
+++ practise.js
@@ -0,0 +1,6 @@
+const id = "55555";
+const issues = [{ id: "55555" }, { id: "666666" }, { id: "111111" }];
+for (const issue of issues) {
+    console.log(issue.id);
+}
+// console.log(issues.filter(issue.id === id));
