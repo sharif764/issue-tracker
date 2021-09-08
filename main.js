f7cbea0685db886fc0f0bad697c3139371d6da55
sharif764 <ss1884088@gmail.com>
2021-09-09 04:05:34 +0600

    JJ

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ function submitIssue(e) {
   fetchIssues();
   e.preventDefault();
 }
-
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const currentIssue = issues.find(issue => issue.id === id);
@@ -39,7 +38,8 @@ const deleteIssue = id => {
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const issuesList = document.getElementById('issuesList');
-  issuesList.innerHTML = '';
+
+  issuesList.innerHTML;
   if (issues) {
     for (var i = 0; i < issues.length; i++) {
       const { id, description, severity, assignedTo, status } = issues[i];
